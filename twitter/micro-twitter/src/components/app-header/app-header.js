3d2f01c731402261e899a7f96c367250727d4c40
Roman Cherkasov <palkamon@yandex.ru>
2022-04-04 23:49:20 +0700

    micro twitter start example

---
+++ twitter/micro-twitter/src/components/app-header/app-header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AppHeader = () => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Roman Cherkasov</h1>
+            <h2>5 Twitts, liked 0</h2>
+        </div>
+    )
+}
+export default AppHeader;
