0e1e1eb313b05d75d10c95375727c334da26b3ce
Roman Cherkasov <palkamon@yandex.ru>
2022-04-11 22:17:48 +0700

    new components

--- twitter/micro-twitter/src/components/app/app.js
+++ twitter/micro-twitter/src/components/app/app.js
@@ -1,30 +1,61 @@
-import React from 'react';
+import React, {Component} from 'react';
 import AppHeader from '../app-header/app-header';
 import SearchPanel from '../search-panel/search-panel';
 import PostStatusFilter from '../post-status-filter/post-status-filter';
 import PostList from '../post-list/post-list';
 import PostAddForm from '../post-add-form/post-add-form';
 import './app.css';
+import styled from 'styled-components';
 
-const App = () => {
+const AppBlock = styled.div`
+    margin: 0 auto;
+    max-width: 800px; 
+`
 
-    const data = [
-        {label: 'Going to learn React', important: true, id: '213'},
-        {label: 'That is work', important: false, id: '2sdf13'},
-        {label: 'May be', important: false, id: '21233'}
-    ];
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data:[
+                {label: 'Going to learn React', important: true, id: '213'},
+                {label: 'That is work', important: false, id: '2sdf13'},
+                {label: 'May be', important: false, id: '21233'}
+            ]
+        };
+        this.deleteItem = this.deleteItem.bind(this);
+        this.addItem = this.addItem.bind(this);
+    }
+    
+    deleteItem(id){
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+            const before = data.slice(0, index);
+            const after = data.slice(index+1);
+            const newArray = [...before, ...after];
+            // const newArr = [...data.slice(0, index), ...data.slice(index+1)] | Это тоже будет работать
+            return {
+                data: newArray
+            }
+        });
+    }
 
-    return (
-        <div className="App container">
-            <AppHeader/>
-            <div className="search-panel d-flex">
-                <SearchPanel/>
-                <PostStatusFilter/>
-            </div>
-            <PostList posts={data} />
-            <PostAddForm/>
-        </div>
-    )
+    addItem(body) {
+        console.log(body)
+    }
+    render(){
+        return (
+            <AppBlock>
+                <AppHeader/>
+                <div className="search-panel d-flex">
+                    <SearchPanel/>
+                    <PostStatusFilter/>
+                </div>
+                <PostList 
+                posts={this.state.data}
+                onDelete={this.deleteItem}/>
+                <PostAddForm
+                onAdd={this.addItem}/>
+            </AppBlock>
+        )
+    }
 }
-
-export default App;
--- twitter/micro-twitter/src/components/post-list/post-list.js
+++ twitter/micro-twitter/src/components/post-list/post-list.js
@@ -1,22 +1,26 @@
 import React from "react";
 import PostListItem from "../post-list-item/post-list-item";
+import { ListGroup  } from "reactstrap";
 import "./post-list.css";
 
-const PostList = ({posts}) => {
+const PostList = ({posts, onDelete}) => {
 
     const elements = posts.map((item) => {
+        const {id, ...itemProps} = item;
         return (
-            <li key={item.id} className="list-group-item">
-                <PostListItem {...item}
+            <li key={id} className="list-group-item">
+                <PostListItem 
+                {...itemProps}
+                onDelete={() => onDelete(id)}
                 />
             </li>
         )
     });
 
     return (
-        <ul className="app-list list-group">
+        <ListGroup  className="app-list">
             {elements}
-        </ul>
+        </ListGroup>
     )
 }
 export default PostList;
